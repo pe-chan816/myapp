ad9527eed244dfcb5683075c82158b065cf0e862
pe-chan816 <bouncing.yamapie816@gmail.com>
2021-01-21 13:27:05 +0900

    creating signup feature for now

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,22 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(signup_params)
+    if @user.save
+      redirect_to user_url(@user)
+    else
+    end
   end
 
   def show
     @user = User.find(params[:id])
   end
+
+  private
+    def signup_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end
