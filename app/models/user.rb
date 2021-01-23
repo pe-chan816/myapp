47942420665ef94a6fec33235f7f7c65f4ee1636
pe-chan816 <bouncing.yamapie816@gmail.com>
2021-01-23 23:31:40 +0900

    Add validates for User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,13 @@
 class User < ApplicationRecord
+  before_save {self.email.downcase!}
+
+  validates :name, presence: true, length:{maximum: 30}
+
+  VALID_EMAIL_REGEX = /\A[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates :email, presence: true, length:{maximum: 255},
+                    format: {with: VALID_EMAIL_REGEX},
+                    uniqueness: {case_sensitive: false}
+
   has_secure_password
+  validates :password, length: {minimum: 8}
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,69 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "バリデーションについて" do
+    let(:user) { User.new(params) }
+    let(:params) {{
+      name: 'test-user',
+      email: 'test-email@email.com',
+      password: 'test_password',
+      password_confirmation: 'test_password'
+    }}
+
+    describe "nameのバリデーション" do
+      it "空欄ではない" do
+        user.name = nil
+        expect(user.valid?).to eq(false)
+      end
+      it "31字以上ではない" do
+        user.name = "a"*31
+        expect(user.valid?).to eq(false)
+      end
+      it "30字以内である" do
+        user.name = "a"*30
+        expect(user.valid?).to eq(true)
+      end
+    end
+
+    describe "emailのバリデーション" do
+      it "空欄ではない" do
+        user.email = nil
+        expect(user.valid?).to eq(false)
+      end
+      it "256字以上ではない" do
+        user.email = "a"*246 + "@email.com"
+        expect(user.valid?).to eq(false)
+      end
+      it "255字以内である" do
+        user.email = "a"*245 + "@email.com"
+        expect(user.valid?).to eq(true)
+      end
+      it "HTMLのメールアドレスの正規表現に近いアドレスである" do
+        expect(user.valid?).to eq(true)
+      end
+      it "@の後ろのドメイン名に'.'が連続していない" do
+        user.email = "email@email..com"
+        expect(user.valid?).to eq(false)
+      end
+      it "重複したメールアドレスは登録できない" do
+        user2 = user.dup
+        user.save
+        expect(user2.valid?).to eq(false)
+      end
+    end
+
+    describe "passwordのバリデーション" do
+      it "8文字以上必要である" do
+        user.password = "a"*8
+        user.password_confirmation = "a"*8
+        expect(user.valid?).to eq(true)
+      end
+      it "7文字以下はダメである" do
+        user.password = "a"*7
+        user.password_confirmation = "a"*7
+        expect(user.valid?).to eq(false)
+      end
+    end
+
+  end
 end
