05f5952eb2683bd664859d8a3052c1a3daa15461
pe-chan816 <bouncing.yamapie816@gmail.com>
2021-05-30 13:42:15 +0900

    [Add] #19 ユーザー検索およびツイート・ユーザー同時検索のテスト追加

--- spec/system/search_system_spec.rb
+++ spec/system/search_system_spec.rb
@@ -1,24 +1,59 @@
 require 'rails_helper'
 
 RSpec.describe "検索機能関連", type: :system do
-  describe "ツイート検索" do
-    before do
-      FactoryBot.create(:testuser)
+  before do
+      @user = FactoryBot.create(:testuser)
       login_as_testuser
       visit root_path
     end
+
+  describe "ツイート検索" do
+    before do
+      @tweet = Tweet.find_by(content: "This is No.1 message.")
+    end
+
     it "正常に機能する（完全一致）" do
-      fill_in 'search_word', with: "This is No.1 message."
+      fill_in 'search_word', with: "#{@tweet.content}"
       click_on 'search'
       expect(current_path).to eq search_path
-      expect(page).to have_content "This is No.1 message."
+      expect(page).to have_content "#{@tweet.content}"
     end
 
     it "正常に機能する（部分一致）" do
       fill_in 'search_word', with: "No.1"
       click_on 'search'
       expect(current_path).to eq search_path
-      expect(page).to have_content "This is No.1 message."
+      expect(page).to have_content "#{@tweet.content}"
+    end
+  end
+
+  describe "ユーザー検索" do
+    it "正常に機能する（完全一致）" do
+      fill_in 'search_word', with: "#{@user.name}"
+      click_on 'search'
+      expect(current_path).to eq search_path
+      expect(page).to have_content "#{@user.name}"
+    end
+
+    it "正常に機能する（部分一致）" do
+      fill_in 'search_word', with: "USER_1"
+      click_on 'search'
+      expect(current_path).to eq search_path
+      expect(page).to have_content "#{@user.name}"
+    end
+  end
+
+  describe "ツイート・ユーザー同時検索" do
+    before do
+      @tweet = Tweet.find_by(content: "I'm TEST_USER_1")
+    end
+
+    it "正常に機能する" do
+      fill_in 'search_word', with: "USER_1"
+      click_on 'search'
+      expect(current_path).to eq search_path
+      expect(page).to have_content "#{@tweet.content}"
+      expect(page).to have_content "#{@user.name}"
     end
   end
 end
