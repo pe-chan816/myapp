22b7e3e80deafe29eb0630f1a1719618b320c826
pe-chan816 <bouncing.yamapie816@gmail.com>
2021-07-17 21:53:32 +0900

    [UPDATE]#90 若干の記述の整理

--- frontend/myapp/src/home/homeContent.tsx
+++ frontend/myapp/src/home/homeContent.tsx
@@ -8,9 +8,6 @@ import useTimeline from "hooks/useTimeline";
 const HomeContent = () => {
   console.log("!!HomeContent!!");
 
-  //const [content, setContent] = useState<TweetType[]>([]);
-  //const [user, setUser] = useState<UserType[]>([]);
-
   const [data, setData] = useState<TimelineType[]>([]);
 
   const getContents = () => {
@@ -18,10 +15,7 @@ const HomeContent = () => {
     const url = `http://localhost:3000`;
     const config = { withCredentials: true };
     axios.get(url, config).then(response => {
-      console.log(response);
       response.data.home_data.forEach((e: TimelineType) => setData(data => [...data, e]));
-      //response.data.users.forEach((e: UserType) => setUser(user => [...user, e]));
-      //response.data.tweets.forEach((e: TweetType) => setContent(content => [...content, e]));
     }).catch(error => {
       console.log("There are something errors", error);
     });
@@ -32,11 +26,11 @@ const HomeContent = () => {
   const Timeline = useTimeline(data);
 
   return (
-    <>
+    <div>
       <button onClick={getContents}>更新</button>
       {data.toString() !== [].toString() &&
         Timeline}
-    </>
+    </div>
   );
 }
 
