3f26cc2ba18d032eb64d407cb9926b2b83f80326
pe-chan816 <bouncing.yamapie816@gmail.com>
2021-07-19 15:48:44 +0900

    [ADD]#97 myFovoriteのテスト追加

---
+++ frontend/myapp/src/__tests__/favorite/myFavorite.test.tsx
@@ -0,0 +1,69 @@
+import { Router } from 'react-router-dom';
+import axios from 'axios';
+import { act, cleanup, render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import MockAdapter from "axios-mock-adapter";
+import { createMemoryHistory } from 'history';
+
+import App from "App";
+
+afterEach(cleanup);
+
+describe("いいね一覧の挙動", () => {
+  const renderLoginSituation = () => {
+    const history = createMemoryHistory();
+    const mock = new MockAdapter(axios);
+    // AppのcheckLoginStatusでのログインチェック
+    mock.onGet("http://localhost:3000/check_login")
+      .reply(200, {
+        logged_in: true,
+        user: { id: 1 }
+      });
+    mock.onGet("http://localhost:3000/users/1/myfavorite")
+      .reply(200, {
+        my_favorites: [
+          {
+            id: 1,
+            content: "とりあえず生",
+            user_id: 1,
+            name: "通りすがりのビール好き",
+            hashname: [{
+              hashname: "ビール"
+            }, {
+              hashname: "ハートランド"
+            }]
+          }, {
+            id: 2,
+            content: "ノンアルコールでお願いします",
+            user_id: 2,
+            name: "Mr.下戸",
+            hashname: [{
+              hashname: "飲めない"
+            }]
+          }]
+      });
+
+    act(() => {
+      render(
+        <Router history={history}>
+          <App />
+        </Router>
+      );
+    });
+  };
+
+  it("各要素が正常に表示されている", async () => {
+    renderLoginSituation();
+    const myFavoriteLink = await screen.findByText("マイいいね");
+    act(() => { userEvent.click(myFavoriteLink) });
+
+    expect(await screen.findByText("いいねしたツイート")).toBeInTheDocument();
+    expect(await screen.findByText("とりあえず生")).toBeInTheDocument();
+    expect(await screen.findByText("通りすがりのビール好き")).toBeInTheDocument();
+    expect(await screen.findByText("#ビール")).toBeInTheDocument();
+    expect(await screen.findByText("#ハートランド")).toBeInTheDocument();
+    expect(await screen.findByText("ノンアルコールでお願いします")).toBeInTheDocument();
+    expect(await screen.findByText("Mr.下戸")).toBeInTheDocument();
+    expect(await screen.findByText("#飲めない")).toBeInTheDocument();
+  });
+});
--- frontend/myapp/src/favorite/myFavorite.tsx
+++ frontend/myapp/src/favorite/myFavorite.tsx
@@ -1,11 +1,10 @@
 import axios from "axios";
-import { useState, useLayoutEffect } from "react";
+import { useState, useEffect } from "react";
 import { useParams } from "react-router";
 
 import { TimelineType } from "types/typeList";
 
 import useTimeline from "hooks/useTimeline";
-
 
 const MyFavorite = (props: any) => {
   console.log(props);
@@ -22,23 +21,8 @@ const MyFavorite = (props: any) => {
     });
   };
 
-  useLayoutEffect(getFavoriteData, []);
+  useEffect(getFavoriteData, []);
 
-  /*
-  const timeline = data.map((e, i) => {
-    if (e) {
-      return (
-        <div key={i}>
-          {e.profile_image?.url && <img src={`http://localhost:3000/${e.profile_image.url}`} alt="user" />}
-          <p>{e.name}</p>
-          <p>{e.content}</p>
-          {e.tweet_image?.url && <img src={`http://localhost:3000/${e.tweet_image.url}`} alt="tweet" />}
-          <p>{e.created_at}</p>
-        </div>
-      );
-    }
-  });
-*/
   const timeline = useTimeline(data);
 
   console.log(data);
