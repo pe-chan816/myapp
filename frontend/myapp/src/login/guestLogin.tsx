244b94b99f2647a65fb939356509cea48f613287
pe-chan816 <bouncing.yamapie816@gmail.com>
2021-07-19 12:22:31 +0900

    [UPDATE] react-routerのuseHistoryを使用した画面遷移に変更

--- frontend/myapp/src/login/guestLogin.tsx
+++ frontend/myapp/src/login/guestLogin.tsx
@@ -1,12 +1,19 @@
+import { LoginStateContext } from "App";
 import axios from "axios";
+import { useContext } from "react";
+import { useHistory } from "react-router-dom";
 
 const GuestLogin = () => {
+  const histroy = useHistory();
+  const { setLoginState } = useContext(LoginStateContext);
   const clickGuestLoginButton = () => {
     const url = `http://localhost:3000/guest`;
     const config = { withCredentials: true };
     axios.get(url, config).then(res => {
       console.log(res);
-      window.location.replace(`http://localhost:8000/`);
+      setLoginState(true);
+      histroy.push("/");
+      //window.location.replace(`http://localhost:8000/`);
     });
   };
 
