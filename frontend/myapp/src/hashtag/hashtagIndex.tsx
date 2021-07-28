ece857d57666679247a25b5fab5220bb4b0791cd
pe-chan816 <bouncing.yamapie816@gmail.com>
2021-07-28 12:39:55 +0900

    [ADD]#131 Material-UIの導入

--- frontend/myapp/src/hashtag/hashtagIndex.tsx
+++ frontend/myapp/src/hashtag/hashtagIndex.tsx
@@ -1,6 +1,8 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import { Link as RouterLink } from "react-router-dom";
+
+import { Grid, Link, makeStyles } from '@material-ui/core';
 
 import { HashtagType } from "types/typeList";
 
@@ -8,14 +10,21 @@ const HashtagIndex = () => {
   console.log("!!hashtagIndex!!");
 
   const [data, setData] = useState<HashtagType[]>([]);
+  const useStyles = makeStyles({
+    base: {
+      margin: "0 auto",
+      maxWidth: "800%",
+      width: "60%"
+    }
+  });
+  const classes = useStyles();
 
   const getIndexData = () => {
-    console.log("!!getIndexData!!");
     setData([]);
     const url = `http://localhost:3000/hashtags`;
     const config = { withCredentials: true };
     axios.get(url, config).then(res => {
-      res.data.hashtags.forEach((e: HashtagType) => setData(data => [...data, e]));
+      setData(res.data.hashtags);
     }).catch(error => {
       console.log("error ->", error);
     });
@@ -24,18 +33,32 @@ const HashtagIndex = () => {
 
   const hashtagIndexContent = data.map((e, i) => {
     return (
-      <div key={i}>
-        <Link to={`/hashtag/${e.hashname}`}>
+      <Grid key={i} item>
+        <Link component={RouterLink} to={`/hashtag/${e.hashname}`}>
           {e.hashname}({e.count})
         </Link>
-      </div>
+      </Grid>
     );
   });
 
   return (
-    <div>
-      <h3>ハッシュタグ一覧</h3>
-      <div>{hashtagIndexContent}</div>
+    <div >
+      <Grid alignItems="center"
+        className={classes.base}
+        container
+        direction="column"
+        justifyContent="flex-start"
+      >
+        <h3>ハッシュタグ一覧</h3>
+        <Grid alignItems="center"
+          container
+          direction="row"
+          justifyContent="center"
+          spacing={1}
+        >
+          {hashtagIndexContent}
+        </Grid>
+      </Grid>
     </div>
   );
 };
