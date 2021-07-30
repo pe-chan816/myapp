fc3aa1af993ad0a4e0fe9fe804d794dc097d301a
pe-chan816 <bouncing.yamapie816@gmail.com>
2021-07-30 12:29:28 +0900

    [UPDATE]テストの記述を修正

--- frontend/myapp/src/__tests__/authentication/login.test.tsx
+++ frontend/myapp/src/__tests__/authentication/login.test.tsx
@@ -11,15 +11,17 @@ import Login from "login/login";
 afterEach(cleanup);
 
 const renderDOM = () => {
-  render(
-    <Login />
-  );
+  act(() => {
+    render(
+      <Login />
+    );
+  });
 };
 
 describe("ログインフォーム", () => {
-  it("各要素が正しく表示されている", () => {
+  it("各要素が正しく表示されている", async () => {
     renderDOM();
-    expect(screen.getByPlaceholderText("メールアドレス")).toBeInTheDocument();
+    expect(await screen.findByPlaceholderText("メールアドレス")).toBeInTheDocument();
     expect(screen.getByPlaceholderText("パスワード")).toBeInTheDocument();
     expect(screen.getByText("ログイン")).toBeInTheDocument();
   });
@@ -31,13 +33,13 @@ describe("ログイン成功時の挙動", () => {
     const mock = new MockAdapter(axios);
     //ログインボタンを押したとき
     mock.onPost("http://localhost:3000/login")
-      .reply(200, { logged_in: true });
-    //"checkLoginStatus"from"App"でのログインチェック
-    mock.onGet("http://localhost:3000/check_login")
       .reply(200, {
         logged_in: true,
-        user: { id: 1 }
-      })
+        user: {
+          id: 1,
+          name: "somebody"
+        }
+      });
     //HomeContent用
     mock.onGet("http://localhost:3000")
       .reply(200,
@@ -61,26 +63,27 @@ describe("ログイン成功時の挙動", () => {
       );
     });
 
-    userEvent.click(screen.getByRole("link", { name: "ログイン" })) //ログイン画面に移動
+    const loginLink = await screen.findByRole("link", { name: "ログイン" });
+    act(() => { userEvent.click(loginLink) });
 
-    //const emailForm = screen.getByRole("textbox", { name: "Email:" });
-    const emailForm = screen.getByPlaceholderText("メールアドレス");
+    const emailForm = await screen.findByPlaceholderText("メールアドレス");
     const passwordForm = screen.getByPlaceholderText("パスワード");
-    const button = screen.getByRole("button", { name: "ログイン" });
+    const loginButton = screen.getByRole("button", { name: "ログイン" });
     act(() => {
       userEvent.type(emailForm, "email@email.com");
       userEvent.type(passwordForm, "password");
-      userEvent.click(button);
+      userEvent.click(loginButton);
     });
 
+    await screen.findByText("マイページ");
     expect(await screen.findByText("ノンアルコールでお願いします")).toBeInTheDocument();
   });
 });
 
 describe("ログイン失敗時の挙動", () => {
-  const failLogin = () => {
-    //const emailForm = screen.getByRole("textbox", { name: "Email" });
-    const emailForm = screen.getByPlaceholderText("メールアドレス");
+  const failLogin = async () => {
+    renderDOM();
+    const emailForm = await screen.findByPlaceholderText("メールアドレス");
     const passwordForm = screen.getByPlaceholderText("パスワード");
     const button = screen.getByRole("button", { name: "ログイン" });
     const mock = new MockAdapter(axios);
@@ -95,19 +98,15 @@ describe("ログイン失敗時の挙動", () => {
   };
 
   it("エラーメッセージが表示される", async () => {
-    act(() => {
-      renderDOM();
-    });
     failLogin();
+
     expect(await screen.findByText("ログイン失敗")).toBeInTheDocument();
   });
 
   it("passwordのvalueがクリアされる", async () => {
-    act(() => {
-      renderDOM();
-    });
     failLogin();
-    //expect(await screen.findByPlaceholderText("パスワード")).toBeInTheDocument();
-    expect(await screen.queryByDisplayValue("wrong_password")).not.toBeInTheDocument();
+
+    await screen.findByText("ログイン失敗")
+    expect(screen.queryByDisplayValue("wrong_password")).not.toBeInTheDocument();
   });
 });
