7541a8fc3ac0ac3a4f5a40b1f1a5f3b62b0bf8f2
pe-chan816 <bouncing.yamapie816@gmail.com>
2021-07-31 22:11:52 +0900

    [ADD]#141 ログイン時のメッセージを表示

--- frontend/myapp/src/__tests__/authentication/login.test.tsx
+++ frontend/myapp/src/__tests__/authentication/login.test.tsx
@@ -6,65 +6,61 @@ import MockAdapter from "axios-mock-adapter";
 import { createMemoryHistory } from 'history';
 
 import App from "App";
-import Login from "login/login";
 
 afterEach(cleanup);
 
-const renderDOM = () => {
+const renderDOM = async () => {
+  const history = createMemoryHistory();
+  const mock = new MockAdapter(axios);
+  //ログインボタンを押したとき
+  mock.onPost("http://localhost:3000/login")
+    .reply(200, {
+      logged_in: true,
+      user: {
+        id: 1,
+        name: "somebody"
+      }
+    });
+  //HomeContent用
+  mock.onGet("http://localhost:3000")
+    .reply(200,
+      {
+        home_data: [{
+          id: 2,
+          content: "ノンアルコールでお願いします",
+          user_id: 2,
+          name: "Mr.下戸",
+          hashname: [{
+            hashname: "飲めない"
+          }]
+        }]
+      });
+
   act(() => {
     render(
-      <Login />
+      <Router history={history}>
+        <App />
+      </Router>
     );
   });
+
+  const loginLink = await screen.findByRole("link", { name: "ログイン" });
+  act(() => { userEvent.click(loginLink) });
 };
 
 describe("ログインフォーム", () => {
   it("各要素が正しく表示されている", async () => {
     renderDOM();
-    expect(await screen.findByPlaceholderText("メールアドレス")).toBeInTheDocument();
+    expect(await screen.findByText("アカウントログイン")).toBeInTheDocument();
+    expect(screen.getByPlaceholderText("メールアドレス")).toBeInTheDocument();
     expect(screen.getByPlaceholderText("パスワード")).toBeInTheDocument();
-    expect(screen.getByText("ログイン")).toBeInTheDocument();
+    expect(screen.getByRole("button", { name: "ログイン" })).toBeInTheDocument();
   });
 });
 
 describe("ログイン成功時の挙動", () => {
-  it("ログイン状態になりHomeHeaderが切り替わる", async () => {
-    const history = createMemoryHistory();
-    const mock = new MockAdapter(axios);
-    //ログインボタンを押したとき
-    mock.onPost("http://localhost:3000/login")
-      .reply(200, {
-        logged_in: true,
-        user: {
-          id: 1,
-          name: "somebody"
-        }
-      });
-    //HomeContent用
-    mock.onGet("http://localhost:3000")
-      .reply(200,
-        {
-          home_data: [{
-            id: 2,
-            content: "ノンアルコールでお願いします",
-            user_id: 2,
-            name: "Mr.下戸",
-            hashname: [{
-              hashname: "飲めない"
-            }]
-          }]
-        });
-
-    act(() => {
-      render(
-        <Router history={history}>
-          <App />
-        </Router>
-      );
-    });
-
-    const loginLink = await screen.findByRole("link", { name: "ログイン" });
-    act(() => { userEvent.click(loginLink) });
+  const renderLogin = async () => {
+    await renderDOM();
 
     const emailForm = await screen.findByPlaceholderText("メールアドレス");
     const passwordForm = screen.getByPlaceholderText("パスワード");
@@ -76,13 +72,22 @@ describe("ログイン成功時の挙動", () => {
     });
 
     await screen.findByText("マイページ");
+  };
+
+  it("ログイン状態になりHomeHeaderが切り替わる", async () => {
+    await renderLogin();
     expect(await screen.findByText("ノンアルコールでお願いします")).toBeInTheDocument();
+  });
+
+  it("メッセージが表示される", async () => {
+    await renderLogin();
+    expect(await screen.findByText("おかえりなさいませ")).toBeInTheDocument();
   });
 });
 
 describe("ログイン失敗時の挙動", () => {
   const failLogin = async () => {
-    renderDOM();
+    await renderDOM();
     const emailForm = await screen.findByPlaceholderText("メールアドレス");
     const passwordForm = screen.getByPlaceholderText("パスワード");
     const button = screen.getByRole("button", { name: "ログイン" });
