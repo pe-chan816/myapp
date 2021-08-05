f1ac56386c0068ba1c6d399c10fe5e3dc1f4e6d4
pe-chan816 <bouncing.yamapie816@gmail.com>
2021-08-05 17:52:19 +0900

    [FIX] myFavoriteが表示されなくなっていた不具合を修正

--- frontend/myapp/src/favorite/myFavorite.tsx
+++ frontend/myapp/src/favorite/myFavorite.tsx
@@ -16,10 +16,15 @@ const MyFavorite = (props: any) => {
   const [page, setPage] = useState<number>(1);
   const [pageNumber, setPageNumber] = useState<number>(1);
   const useStyles = makeStyles({
-    base: {
+    pagination: {
+      alignItems: "center",
+      display: "flex",
+      justifyContent: "center",
       margin: "0 auto",
-      maxWidth: "600px",
-      width: "100%"
+      maxWidth: "600px"
+    },
+    text: {
+      textAlign: "center"
     }
   });
   const classes = useStyles();
@@ -49,7 +54,7 @@ const MyFavorite = (props: any) => {
 
   const MyPagination = () => {
     return (
-      <Pagination
+      <Pagination className={classes.pagination}
         color="primary"
         count={pageNumber}
         onChange={(e, p) => handlePagination(p)}
@@ -60,25 +65,15 @@ const MyFavorite = (props: any) => {
   }
 
   return (
-    <Grid container
-      className={classes.base}
-      direction="column"
-      justifyContent="center"
-      alignItems="center"
-    >
-      <Grid item >
-        <h3>いいねしたポスト</h3>
-      </Grid>
+    <div>
+      <h3 className={classes.text}>いいねしたポスト</h3>
 
-      <Grid className={classes.base} item>
-        <Timeline data={data} />
-      </Grid>
-
-      <Grid item>
-        <MyPagination />
-      </Grid>
-
-    </Grid>
+      {data.toString() !== [].toString() &&
+        <>
+          <Timeline data={data} />
+          <MyPagination />
+        </>}
+    </div>
   );
 };
 
