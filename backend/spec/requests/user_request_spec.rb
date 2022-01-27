3574c1f3998323b65c60e077501f468481a85a9c
pe-chan816 <bouncing.yamapie816@gmail.com>
2022-01-27 21:55:28 +0900

    requestスペックはさしあたり不要なので削除

--- backend/spec/requests/user_request_spec.rb
+++ backend/spec/requests/user_request_spec.rb
@@ -1,44 +1,4 @@
 require 'rails_helper'
 
 RSpec.describe "Users", type: :request do
-  include SessionsHelper
-  let(:user) {FactoryBot.create(:testuser)}
-
-  describe "new メソッド" do
-    describe "GET /signup" do
-      it "正しくページを開ける" do
-        get "/signup"
-        expect(response).to have_http_status(:success)
-      end
-    end
-  end
-
-  describe "update メソッド" do
-    describe "PATCH /users/:id" do
-      it  "ログインしていないと更新できない" do
-        patch '/users/user.id', params:{name:'Another Man'}
-        expect(response).not_to have_http_status(:success)
-      end
-      it "admin属性は変更できない" do
-        patch '/users/user.id', params:{admin: :true}
-        expect(response).not_to have_http_status(:success)
-      end
-    end
-  end
-
-  describe "show メソッド" do
-    describe "ユーザーの個人ページ" do
-      it "正しく開ける" do
-        # SessionsHelper が view で絡むので systemspec でカバーする
-      end
-    end
-  end
-
-  describe "index メソッド" do
-    it "ログインしていない場合リダイレクトされる" do
-      # RequestSpec では session が取り扱えないのでログイン中の挙動は login_system_spec.rb に書く
-      get users_path
-      expect(response).to have_http_status(:redirect)
-    end
-  end
 end
