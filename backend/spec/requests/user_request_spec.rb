8c80573387e88252ca27932c3def0e55dea14829
pe-chan816 <bouncing.yamapie816@gmail.com>
2022-01-30 12:56:58 +0900

    user#show requestスペック作成

--- backend/spec/requests/user_request_spec.rb
+++ backend/spec/requests/user_request_spec.rb
@@ -13,8 +13,10 @@ RSpec.describe "Users", type: :request do
       end
 
       it "作成したユーザー情報が返ってくる" do
-        expect(@json['user']).to include("name" => "TEST_USER_1",
-                                         "email" => "email@email.com")
+        expect(@json['user']).to include(
+          "name" => "TEST_USER_1",
+          "email" => "email@email.com"
+        )
       end
 
       it "ランダムな英数字のunique_nameが設定されている" do
@@ -96,6 +98,65 @@ RSpec.describe "Users", type: :request do
         expect(@json['messages'][0]).to eq "Nameは30文字以内で入力してください"
       end
     end
+  end
 
+
+  describe "users#show" do
+    before do
+      FactoryBot.create(:relationship)
+      @user = User.find_by( name: "TEST_USER_1" )
+      @user2 = User.find_by( name: "TEST_USER_2" )
+      login_as_testuser
+      get user_url(@user)
+
+      @json = JSON.parse(response.body)
+    end
+
+    it "ユーザー情報が返ってくる" do
+      expect(@json['user']).to include(
+        "email" => @user.email,
+        "id" => @user.id,
+        "name" => @user.name
+      )
+    end
+
+    it "ツイート情報が返ってくる" do
+      expect(@json['mypage_data'][0]).to include(
+        "content" => "This is No.30 message.",
+        "name" => @user.name,
+        "user_id" => @user.id
+      )
+    end
+
+    it "フォロー情報が返ってくる" do
+      expect(@json['followings'][0]).to include(
+        "email" => @user2.email,
+        "id" => @user2.id,
+        "name" => @user2.name
+      )
+    end
+
+    it "フォロー人数が返ってくる" do
+      expect(@json['followings_count']).to eq @user.following.count
+    end
+
+    it "フォロワー情報が返ってくる" do
+      get user_url(@user2)
+      json = JSON.parse(response.body)
+
+      expect(json['followers'][0]).to include(
+        "email" => @user.email,
+        "id" => @user.id,
+        "name" => @user.name
+      )
+    end
+
+    it "フォロワー人数が返ってくる" do
+      expect(@json['followers_count']).to eq @user.followers.count
+    end
+
+    it "フォローしてるかどうかの情報が返ってくる" do
+      expect(@json['follow_or_not']).to eq @user.following?(@user)
+    end
   end
 end
