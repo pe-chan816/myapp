276553a9e193ca405ed7a3dd725af6f134e952b9
pe-chan816 <bouncing.yamapie816@gmail.com>
2022-02-01 08:39:18 +0900

    sessions#create requestスペック作成

--- backend/spec/requests/sessions_request_spec.rb
+++ backend/spec/requests/sessions_request_spec.rb
@@ -1,11 +1,53 @@
 require 'rails_helper'
 
 RSpec.describe "Sessions", type: :request do
-  describe "new メソッド" do
-    describe "GET /login" do
-      it "正常にページを開ける" do
-        get "/login"
-        expect(response).to have_http_status(:success)
+  describe "sessions#create" do
+    let(:user) { FactoryBot.create(:testuser) }
+
+    context "認証に成功" do
+      before do
+        user
+        post login_url, params: { user: {
+          email: user.email,
+          password: "password"
+        }}
+
+        @json = JSON.parse(response.body)
+      end
+
+      it "ログインステータスが返ってくる" do
+        expect(@json['logged_in']).to eq true
+      end
+
+      it "ユーザー情報が返ってくる" do
+        expect(@json['user']).to include(
+          "email" => user.email,
+          "id" => user.id,
+          "name" => user.name
+        )
+      end
+    end
+
+    context "認証に失敗" do
+      before do
+        user
+        post login_url, params: { user: {
+          email: user.email,
+          password: "false_password"
+        }}
+
+        @json = JSON.parse(response.body)
+      end
+
+      it "ステータスコード401が返ってくる" do
+        expect(@json['status']).to eq 401
+      end
+
+      it "メッセージが返ってくる" do
+        expect(@json['errors']).to include(
+          "認証に失敗しました。",
+          "正しいメールアドレス・パスワードを入力し直すか、新規登録を行ってください。"
+        )
       end
     end
   end
