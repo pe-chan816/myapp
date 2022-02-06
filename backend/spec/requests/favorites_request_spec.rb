1db67ac723ab2f71041e7fa023f3c70b9af5e96e
pe-chan816 <bouncing.yamapie816@gmail.com>
2022-02-06 09:44:41 +0900

    favorites#create requestスペック作成

--- backend/spec/requests/favorites_request_spec.rb
+++ backend/spec/requests/favorites_request_spec.rb
@@ -1,5 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe "Favorites", type: :request do
+  let(:user1) { FactoryBot.create(:testuser) }
+  let(:user2) { FactoryBot.create(:testuser2) }
 
+  describe "favorites#create" do
+    def create_favorite
+      post favorites_url, params: {
+        favorite_tweet_id: user2.tweets.first.id
+      }
+    end
+
+    before do
+      user1
+      user2
+      login_as_testuser
+    end
+
+    it "メッセージが返ってくる" do
+      create_favorite
+      json = JSON.parse(response.body)
+
+      expect(json['message']).to eq "いいね！"
+    end
+
+    it "対象ツイートのいいね数が増加する" do
+      expect{
+        create_favorite
+      }.to change{ user2.tweets.first.user_favorited.count }.by(1)
+    end
+  end
 end
